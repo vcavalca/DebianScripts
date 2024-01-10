9ff9d7ba4fcb8710ae40c282e89bf511e9dbd14f
V <61808793+vcavalca@users.noreply.github.com>
2024-01-10 10:52:54 -0300

    Update install_brave.sh

--- install_brave.sh
+++ install_brave.sh
@@ -15,6 +15,16 @@ clear_screen() {
     clear
 }
 
+# Function to display a colored message based on success or failure.
+display_message() {
+    if [ "$1" -eq 0 ]; then
+        echo -e "\e[32m$2\e[0m"  # Success message in green
+    else
+        echo -e "\e[31m$3\e[0m"  # Failure message in red
+        exit 1
+    fi
+}
+
 # Check if sudo is installed.
 if ! command -v sudo &> /dev/null; then
     clear_screen
@@ -24,9 +34,9 @@ if ! command -v sudo &> /dev/null; then
     clear_screen
     echo "It will be necessary to reboot the system to apply the group changes."
     press_enter_to_continue
-    echo "Please re-run the script after logging out and logging back in to apply group changes."
+    echo "Please re-run the script after restarting to apply the group changes."
     press_enter_to_continue
-    reboot
+    exit 0
 fi
 
 # Check if curl is installed.
@@ -34,12 +44,8 @@ if ! command -v curl &> /dev/null; then
     clear_screen
     echo "The 'curl' package is not installed. Installing..."
     press_enter_to_continue
-    if sudo apt install -y curl; then
-        echo -e "\e[32mDone\e[0m"  # Exibe "Done" em verde
-    else
-        echo "Failed to install 'curl'. Exiting..."
-        exit 1
-    fi
+    sudo apt install -y curl
+    display_message $? "Successfully installed" "Failed to install 'curl'. Exiting..."
 fi
 
 # Ensure the script is being run as sudoer.
@@ -54,47 +60,31 @@ fi
 clear_screen
 echo "Downloading and adding Brave Browser keyring..."
 press_enter_to_continue
-if sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg; then
-    echo -e "\e[32mDone\e[0m"  # Exibe "Done" em verde
-else
-    echo "Failed to download and add Brave Browser keyring. Exiting..."
-    exit 1
-fi
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+display_message $? "Successfully installed" "Failed to download and add Brave Browser keyring. Exiting..."
 
 # Add Brave Browser repository.
 clear_screen
 echo "Adding Brave Browser repository..."
 press_enter_to_continue
-if echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list; then
-    echo -e "\e[32mDone\e[0m"  # Exibe "Done" em verde
-else
-    echo "Failed to add Brave Browser repository. Exiting..."
-    exit 1
-fi
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+display_message $? "Successfully installed" "Failed to add Brave Browser repository. Exiting..."
 
 # Update package list.
 clear_screen
 echo "Updating the package list..."
 press_enter_to_continue
-if sudo apt update; then
-    echo -e "\e[32mDone\e[0m"  # Exibe "Done" em verde
-else
-    echo "Failed to update the package list. Exiting..."
-    exit 1
-fi
+sudo apt update
+display_message $? "Successfully installed" "Failed to update the package list. Exiting..."
 
 # Install Brave Browser.
 clear_screen
 echo "Installing Brave Browser..."
 press_enter_to_continue
-if sudo apt install -y brave-browser; then
-    echo -e "\e[32mDone\e[0m"  # Exibe "Done" em verde
-else
-    echo "Failed to install Brave Browser. Exiting..."
-    exit 1
-fi
+sudo apt install -y brave-browser
+display_message $? "Successfully installed" "Failed to install Brave Browser. Exiting..."
 
 # Display a message indicating successful installation.
 clear_screen
-echo -e "\e[32mBrave Browser installation complete.\e[0m"  # Exibe a mensagem em verde
+echo -e "\e[32mBrave Browser installation complete.\e[0m"  # Success message in green
 press_enter_to_continue
